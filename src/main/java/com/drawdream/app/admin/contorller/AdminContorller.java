c498a0deeec02e263ae131afd021baadbc2cfb60
coolxiaohao <1779084072@qq.com>
2020-03-28 12:24:32 +0800

    init

--- src/main/java/com/drawdream/app/admin/contorller/AdminContorller.java
+++ src/main/java/com/drawdream/app/admin/contorller/AdminContorller.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import redis.clients.jedis.Jedis;
-
 @RestController
 @RequestMapping("/admin")
 public class AdminContorller {
