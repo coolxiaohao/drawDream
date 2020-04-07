eec5aa69b6b1389880dc9ec0d1a2c7474f01b973
coolxiaohao <1779084072@qq.com>
2020-04-07 21:59:57 +0800

    update sql

--- sql/drawdream.sql
+++ sql/drawdream.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : 本地mysql
-Source Server Version : 100108
+Source Server         : 本地
+Source Server Version : 50728
 Source Host           : localhost:3306
 Source Database       : drawdream
 
 Target Server Type    : MYSQL
-Target Server Version : 100108
+Target Server Version : 50728
 File Encoding         : 65001
 
-Date: 2020-04-03 16:30:56
+Date: 2020-04-07 21:58:34
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,21 +23,20 @@ CREATE TABLE `admin` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'admin_id',
   `adminName` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '' COMMENT '管理员名称',
   `adminPwd` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '' COMMENT '管理员密码',
-  `adminKey` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '密码密钥',
   `adminActionPwd` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '管理员操作密码',
-  `adminActionKey` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '管理员操作密钥',
   `adminPhone` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '管理员联系电话',
   `adminToken` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '管理员登陆Token',
   `adminRuleId` int(11) DEFAULT NULL COMMENT '管理员权限',
   `addTime` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '添加日期',
   `editTime` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '修改日期',
+  `headPortrait` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '头像',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of admin
 -- ----------------------------
-INSERT INTO `admin` VALUES ('1', 'admin', '123456', '1111', '123456', '1111', '13113100604', null, null, '2020-03-27 09:05:47', '2020-03-28 09:05:54');
+INSERT INTO `admin` VALUES ('1', 'admin', 'e10adc3949ba59abbe56e057f20f883e', 'e10adc3949ba59abbe56e057f20f883e', '13537430256', null, '1', '2020-04-07 21:18:19', '2020-04-07 21:18:19', null);
 
 -- ----------------------------
 -- Table structure for log
@@ -66,7 +65,7 @@ CREATE TABLE `rule` (
   `accessPath` varchar(255) CHARACTER SET utf8 NOT NULL,
   `needRule` int(4) DEFAULT '0' COMMENT '是否需要访问限制（0.需要,1.不需要）',
   `type` int(4) DEFAULT '1' COMMENT '是否启用（1.启用，2.禁用）',
-  `addTime` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '添加时间',
+  `addTime` datetime DEFAULT NULL COMMENT '添加时间',
   `port` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '权限属于哪一个端（‘admin’,‘home’,''api''）',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
@@ -74,7 +73,7 @@ CREATE TABLE `rule` (
 -- ----------------------------
 -- Records of rule
 -- ----------------------------
-INSERT INTO `rule` VALUES ('1', '后台登录', '/admin/login', '1', '1', '2020-04-03 07:55:17', 'admin');
+INSERT INTO `rule` VALUES ('1', '后台登录', '/admin/login', '1', '0', '2020-04-07 19:23:43', 'admin');
 
 -- ----------------------------
 -- Table structure for rulegroup
@@ -89,11 +88,12 @@ CREATE TABLE `rulegroup` (
   `addTime` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '添加时间',
   `editTime` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of rulegroup
 -- ----------------------------
+INSERT INTO `rulegroup` VALUES ('1', '默认', '0', '1', '1', '2020-04-07 19:25:27', '2020-04-07 19:25:32');
 
 -- ----------------------------
 -- Table structure for user
